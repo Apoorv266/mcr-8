239983d3ab31371445ec356963540ced7db0ba71
Apoorv Sharma <apoorvsharma494@gmail.com>
2023-07-14 13:01:55 +0530

    commit

--- src/Utils/UtilFunc.js
+++ src/Utils/UtilFunc.js
@@ -1,20 +1,20 @@
-export const compareDate = (date) =>{
+export const compareDate = (date) => {
     var d1 = new Date();
     var d2 = new Date(date);
-    
+
     return d1.getDate() === d2.getDate();
 }
 
-export const getDateTimeFunc = (str) =>{
-const dateTime = new Date(str);
-const year = dateTime.getFullYear();
-const month = dateTime.getMonth() + 1; // Months are zero-based, so we add 1
-const day = dateTime.getDate();
-const hours = dateTime.getHours();
-const minutes = dateTime.getMinutes();
-const formattedMinutes = minutes.toString().padStart(2, "0");
-const amOrPm = hours >= 12 ? "PM" : "AM";
+export const getDateTimeFunc = (str) => {
+    const dateTime = new Date(str);
+    const year = dateTime.getFullYear();
+    const month = dateTime.getMonth() + 1; // Months are zero-based, so we add 1
+    const day = dateTime.getDate();
+    const hours = dateTime.getHours();
+    const minutes = dateTime.getMinutes();
+    const formattedMinutes = ("0" + minutes).slice(-2);
+    const amOrPm = hours >= 12 ? "PM" : "AM";
 
-return {date: `${year}-${month}-${day}`, time : `${hours}:${formattedMinutes} ${amOrPm}`   }
+    return { date: `${year}-${month}-${day}`, time: `${hours}:${formattedMinutes} ${amOrPm}` }
 
 }
